eab57bb567e8a2965bc26d63e3b8fd77b12179aa
Monicat263 <mda263@bol.com.br>
2025-08-06 11:28:24 -0300

    Inclusão de spring security

---
+++ src/main/java/com/javinha/usuario/infrastructure/entity/Telefone.java
@@ -0,0 +1,25 @@
+package com.javinha.aprendendospring.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "telefone")
+public class Telefone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "numero", length = 10)
+    private String numero;
+    @Column(name = "ddd", length = 3)
+    private String ddd;
+
+}
